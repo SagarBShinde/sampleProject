a383b5b8fdc101193eec35d3ca777a41298c5016
SagarBShinde <sagarshinde3258@gmail.com>
2018-07-21 19:24:29 +0530

    Update SampleProgram.java

--- sampleProject/src/main/java/sampleProject/SampleProgram.java
+++ sampleProject/src/main/java/sampleProject/SampleProgram.java
@@ -5,7 +5,7 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hi World. THis is a change");
+		System.out.println("Hello World. THis is a change");
 		System.out.println("This is a Test GIT push");
 
 	}
