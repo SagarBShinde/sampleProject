b52c407cb5b5f1c072e20ad19f1d20f757880c32
SagarBShinde <sagarshinde3258@gmail.com>
2018-07-21 18:42:28 +0530

    This is a first commit

---
+++ sampleProject/src/main/java/sampleProject/SampleProgram.java
@@ -0,0 +1,12 @@
+package sampleProject;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
